40f574dcffe08f3ded576f96735254ce373119b1
Jessica Howard <jessicahoward1031@gmail.com>
2018-09-19 13:17:49 -0400

    add redux

--- search-party/src/index.js
+++ search-party/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom' //don't need to specify localhost url in axios http address
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
