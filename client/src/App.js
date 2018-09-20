4789e920e12de5cc7f2f35dd6b0b7f276e628f7a
Bernadette Januska <bmjanuska@gmail.com>
2018-09-20 12:31:04 -0400

    Map added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,100 @@
 import React, { Component } from 'react';
 import './App.css';
+import Map from "./components/Map"
 
 class App extends Component {
-  state = {users: []}
+  state = { users: [] }
 
   componentDidMount() {
     fetch('/users')
       .then(res => res.text())
       .then(text => console.log(text))
       .then(users => this.setState({ users }));
+
+    if (navigator.geolocation) {
+      /* geolocation is available */
+      navigator.geolocation.watchPosition(position => {
+        console.log(position);
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        this.setState({ pos, isMarkerShown: true })
+        // panTo(pos); 
+        // Map.setPosition(pos)
+        // Map.setContent('Location Here. ')
+        // Map.setCenter(pos)
+      })
+    } else {
+      /* geolocation IS NOT available */
+    }
   }
 
   render() {
     return (
       <div className="App">
+        <Map
+          pos={this.state.pos}
+          isMarkerShown={this.state.isMarkerShown}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyApIn7dZQPu26fDdHQtu1UMfTOWB7P06B0"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+
         <h1>Users</h1>
-        {this.state.users.map(user =>
+        {/* {this.state.users.map(user =>
           <div key={user.id}>{user.username}</div>
-        )}
+        )} */}
       </div>
     );
   }
 }
 
 export default App;
+
+
+// class App extends Component {
+//   state = { 
+//     pos:{},
+//     isMarkerShown: false
+//   }
+//   componentDidMount() {
+//     if (navigator.geolocation) {
+//         /* geolocation is available */
+//         navigator.geolocation.watchPosition (position => {
+//           console.log(position);
+//           var pos = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude
+//           }
+//           this.setState({pos, isMarkerShown:true})
+//           // panTo(pos); 
+//           // Map.setPosition(pos)
+//           // Map.setContent('Location Here. ')
+//           // Map.setCenter(pos)
+//         })
+//     } else {
+//       /* geolocation IS NOT available */
+//     }
+//   }
+
+
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <Map 
+//           pos={this.state.pos}
+//           isMarkerShown={this.state.isMarkerShown}
+//           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyApIn7dZQPu26fDdHQtu1UMfTOWB7P06B0"
+//           loadingElement={<div style={{ height: `100%` }} />}
+//           containerElement={<div style={{ height: `400px` }} />}
+//           mapElement={<div style={{ height: `100%` }} />}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
