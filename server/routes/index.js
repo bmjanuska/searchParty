2fa07678318e4ea05d623da3fe233691086dff54
Jessica Howard <jessicahoward1031@gmail.com>
2018-09-23 18:01:51 -0400

    new errors

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require("../models");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-router.get('/signup', function(req, res, next) 
+router.post('/signup', function(req, res, next) 
 {
-  console.log("sanity");
-  Users.create({ username: req.body.username, password: req.body.password })
-  .then(() => User.findOrCreate({where: {username: req.body.username}}))
-  .spread((user, created) => {
-    console.log(user.get({
-      plain: true
-    }))
-    console.log(created)
-  }) 
+  console.log("sanity: " + req.body.username);
+  db.Users.create({ 
+    username: req.body.username, 
+    password: req.body.password 
+  })
+  .then(function(dbUser) {
+    res.json(dbUser);
+  })
+  
+  
+  
+  // .then(() => db.Users.findOrCreate({where: {username: req.body.username}}))
+  // .spread((user, created) => {
+  //   console.log(user.post({
+  //     plain: true
+  //   }))
+  //   console.log(created)
+  // }) 
 });
 
 module.exports = router;
