d0206f806c2f8b237a1f4a28198fa16f88a872a6
Bernadette Januska <bmjanuska@gmail.com>
2018-09-24 14:47:50 -0400

    added some buttons

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -23,6 +23,27 @@ class User extends Component {
                         <Col size="12">
                             <h2>Accomplishments</h2>
                         </Col>
+                        <Col size="12">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at quam bibendum, sodales elit ac, volutpat dui. Ut rutrum purus ipsum, ut mollis tellus facilisis non. Fusce viverra commodo luctus. Pellentesque ullamcorper arcu quis velit pulvinar, a convallis nisl pellentesque. Mauris eget arcu orci. Integer eget orci pellentesque, auctor erat eget, malesuada nisi. Sed bibendum tellus a sagittis auctor. Vivamus turpis quam, placerat non arcu at, posuere ultrices nibh. Duis ut commodo nunc. Etiam commodo blandit lobortis. Aliquam fermentum convallis consectetur. Quisque finibus metus ac sem sollicitudin vehicula. Cras gravida iaculis nisi, at mollis dui molestie vitae.</p>
+                        </Col>
+                        <Col size="12">
+                            <button className="btn btn-success">
+                                Search
+                            </button>
+                            <button className="btn btn-purple">
+                                Search
+                            </button>
+                            <button className="btn btn-pink">
+                                Search
+                            </button>
+                            <button className="btn btn-blue">
+                                Search
+                            </button>
+                            <button className="btn btn-yellow">
+                                Search
+                            </button>
+
+                        </Col>
                     </Row>
                     {/* Challenges */}
                 </Container>
