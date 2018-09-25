d30a766d5228b3091a964f62c0889ab08f31e1a9
Bernadette Januska <bmjanuska@gmail.com>
2018-09-24 20:40:09 -0400

    styled search with col and row

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,16 +1,54 @@
 import React from "react";
 import "./SearchResults.css";
+import { Row, Col } from "../Grid";
 
 
 const SearchResults = props => (
-    <span className="search-results">
-      <hr></hr>
-      {/* <p>Name: {props.long_name}</p> */}
-      <h4>Address: {props.formatted_address}</h4>
-      <h4>Lat: {props.lat}</h4>
-      <h4>Lng: {props.lng}</h4>
-      {/* button to add to your challenge */}
+  <div>
+    <hr></hr>
+    <span className="search-results card">
+      <div className="card-body">
+        <Row>
+          <Col size="sm-12 md-9">
+            <h5 className="card-title">Address: </h5>
+            <p className="card-text">{props.formatted_address}</p>
+
+            <h5 className="card-title">Lat: </h5>
+            <p className="card-text">{props.lat}</p>
+
+            <h5 className="card-title">Lng: </h5>
+            <p className="card-text">{props.lng}</p>
+            <br></br>
+          </Col>
+          <Col size="sm-12 md-3">
+            <button href="#" className="btn btn-purple text-right">Add to Challenge</button>
+          </Col>
+        </Row>
+      </div>
     </span>
+  </div>
 );
 
 export default SearchResults;
+
+
+{/* <span className="search-results card">
+<div className="card-body">
+<h5>Address: {props.formatted_address}</h5>
+<h5>Lat: {props.lat}</h5>
+<h5>Lng: {props.lng}</h5>
+<a href="#" class="btn btn-purple">Add to a Challenge</a>
+</div>
+</span> */}
+
+
+{/* <Row>
+  <Col size="md-8">
+    <span><h5>Address:</h5> {props.formatted_address}</span>
+    <span><h5>Lat:</h5> {props.lat}</span>
+    <span><h5>Lng:</h5> {props.lng}</span>
+  </Col>
+  <Col size="md-4">
+    <a href="#" class="btn btn-purple">Add to a Challenge</a>
+  </Col>
+</Row> */}
