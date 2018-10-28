785412013df5a722c7ef4531cc880361f5c192a1
Bernadette Januska <bmjanuska@gmail.com>
2018-10-28 15:09:45 -0400

    auth working new readme

--- client/src/components/Bars/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./TopNavbar";
-export * from "./Hbar";
-export * from "./Footer";
--- client/src/components/Grid/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./grid.css";
-
-// This Container component allows us to use a bootstrap container without worrying about class names
-export const Container = ({ fluid, children }) => (
-  <div className={`container${fluid ? "-fluid" : ""} full`}>
-    {children}
-  </div>
-);
